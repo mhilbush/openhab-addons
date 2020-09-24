4e95c9ef75bbe1e5506125f86f82a9245e49c246
Mark Hilbush <mark@hilbush.com>
2020-09-24 13:25:20 -0400

    SunsetWx binding
    Signed-off-by: Mark Hilbush <mark@hilbush.com>

---
+++ bundles/org.openhab.binding.sunsetwx/src/main/java/org/openhab/binding/sunsetwx/internal/model/QualityConverter.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.sunsetwx.internal.model;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Convert the quality value to a quality string based on the conversion
+ * rules defined by SunsetWx.
+ *
+ * Sunrise
+ * (0-25%) Poor: Less than or equal to -223
+ * (25-50%) Fair: Greater than -223 to -40
+ * (50-75%) Good: Greater than -40 to 142
+ * (75-100%) Great: Greater than 142
+ *
+ * Sunset
+ * (0-17.63%) Poor: Less than or equal to -262.75
+ * (17.63-50%) Fair: Greater than -262.75 to -75
+ * (50-75%) Good: Greater than -75 to 70
+ * (75-100%) Great: Greater than 70
+ *
+ * @author Mark Hilbush - Initial contribution
+ */
+@NonNullByDefault
+public class QualityConverter {
+    ModelType type;
+
+    public QualityConverter(ModelType type) {
+        this.type = type;
+    }
+
+    public QualityType convertQuality(double qualityValue) {
+        if (type.equals(ModelType.SUNRISE)) {
+            if (qualityValue <= -223.0d) {
+                return QualityType.POOR;
+            } else if (qualityValue > -223.0d && qualityValue <= -40.0d) {
+                return QualityType.FAIR;
+            } else if (qualityValue > -40.0d && qualityValue <= 142.0d) {
+                return QualityType.GOOD;
+            } else {
+                return QualityType.GREAT;
+            }
+        } else {
+            if (qualityValue <= -262.75d) {
+                return QualityType.POOR;
+            } else if (qualityValue > -262.75d && qualityValue <= -75.0d) {
+                return QualityType.FAIR;
+            } else if (qualityValue > -75.0d && qualityValue <= 70.0d) {
+                return QualityType.GOOD;
+            } else {
+                return QualityType.GREAT;
+            }
+        }
+    }
+}
---
+++ bundles/org.openhab.binding.sunsetwx/src/main/java/org/openhab/binding/sunsetwx/internal/model/QualityType.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.sunsetwx.internal.model;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * The {@link QualityType} defines the quality levels supported by the service
+ *
+ * @author Mark Hilbush - Initial contribution
+ */
+@NonNullByDefault
+public enum QualityType {
+    POOR("Poor"),
+    FAIR("Fair"),
+    GOOD("Good"),
+    GREAT("Great");
+
+    public final String label;
+
+    private QualityType(String label) {
+        this.label = label;
+    }
+}
